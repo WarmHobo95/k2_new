0b9e1af4f30746b1ba1511b9fd8f78fc4bd3f6b6
pc <a@mail>
2023-10-07 09:40:48 +0500

    rep_create

---
+++ bin/k2_new.dart
@@ -0,0 +1,5 @@
+import 'package:k2_new/k2_new.dart' as k2_new;
+
+void main(List<String> arguments) {
+  print('Hello world: ${k2_new.calculate()}!');
+}
---
+++ test/k2_new_test.dart
@@ -0,0 +1,8 @@
+import 'package:k2_new/k2_new.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
